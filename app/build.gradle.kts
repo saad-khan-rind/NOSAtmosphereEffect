c73fc1dfcf1c6000f0b5abb09040704f5e123379
Saad Ullah Khan <khansaad45678900@gmail.com>
2025-12-20 20:08:38 +0300

    fix comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.app.nosatmosphereeffect"
-        minSdk = 36 // Android 14+
+        minSdk = 36 // Android 16+
         targetSdk = 36
         versionCode = 2
         versionName = "1.0.1"
