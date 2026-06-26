bf1e02cf222f8da3eb9e820660bcc99a12bdc9bb
saad-khan-rind <khansaad45678900@gmail.com>
2026-06-26 05:09:46 +0200

    version Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.saad_khan_rind.atmosphere_effect"
-        versionName = "6.0.1"
-        versionCode = 200601
+        versionName = "6.0.5"
+        versionCode = 200605
     }
 
     flavorDimensions += "apiLevel"
@@ -21,14 +21,14 @@ android {
             dimension = "apiLevel"
             minSdk = 36
             targetSdk = 36
-            versionCode = 200601
+            versionCode = 200605
         }
 
         create("v33") {
             dimension = "apiLevel"
             minSdk = 33
             targetSdk = 33
-            versionCode = 100601
+            versionCode = 100605
         }
 
     }
