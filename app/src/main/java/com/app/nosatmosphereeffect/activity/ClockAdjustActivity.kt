61103720e5464fe8a54e1b6332d418b90f03cb23
saad-khan-rind <khansaad45678900@gmail.com>
2026-08-31 09:32:42 +0200

    temp changes

--- app/src/main/java/com/app/nosatmosphereeffect/activity/ClockAdjustActivity.kt
+++ app/src/main/java/com/app/nosatmosphereeffect/activity/ClockAdjustActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.onSizeChanged
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
